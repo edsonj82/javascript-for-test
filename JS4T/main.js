7c20b41e0e3e49d7bed6d6c24d5ad8697b020956
Edson <edsonj82@outlook.com>
2020-02-22 22:10:00 -0300

    Applying commands and elements javascript

---
+++ JS4T/main.js
@@ -0,0 +1,3 @@
+var welcome = ".::Hello Javscript - Automação Full Stack from QA Ninja::."
+document.getElementById("welcome").append(welcome);
+console.log("Hello JavaScript - Automação Full Stack from QA Ninja");
