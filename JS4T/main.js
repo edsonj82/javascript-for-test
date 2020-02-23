f54f152b7bdc598007032059e4bc58759d5c10fe
Edson <edsonj82@outlook.com>
2020-02-23 12:24:38 -0300

    Variables

--- JS4T/main.js
+++ JS4T/main.js
@@ -1,3 +1,18 @@
 var welcome = ".::Hello Javscript - Automação Full Stack from QA Ninja::."
 document.getElementById("welcome").append(welcome);
-console.log("Hello JavaScript - Automação Full Stack from QA Ninja");
+
+//Variavel eh um local na memória do computador aonde você pode alocar valores
+//que poderão ser alterados durante a execução do software
+
+//tipagem dinâmica, ou seja, atribui o tipo quado recebe o valor
+//typeof eh responsável por trazer o tipo de variavel
+var number = 10;
+console.log(number + " - " + typeof number);
+
+var name = "Fernando";
+console.log(name + " - " + typeof name);
+
+var n1 = 10;
+var n2 = 20;
+n1 = "1" + 5;
+console.log(n1 + n2);
