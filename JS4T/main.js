738f38f95806791f096c673bba3b799673691c85
Edson <edsonj82@outlook.com>
2020-02-23 12:42:06 -0300

    Operators

--- JS4T/main.js
+++ JS4T/main.js
@@ -1,18 +1,5 @@
 var welcome = ".::Hello Javscript - Automação Full Stack from QA Ninja::."
 document.getElementById("welcome").append(welcome);
+console.log(welcome);
 
-//Variavel eh um local na memória do computador aonde você pode alocar valores
-//que poderão ser alterados durante a execução do software
 
-//tipagem dinâmica, ou seja, atribui o tipo quado recebe o valor
-//typeof eh responsável por trazer o tipo de variavel
-var number = 10;
-console.log(number + " - " + typeof number);
-
-var name = "Fernando";
-console.log(name + " - " + typeof name);
-
-var n1 = 10;
-var n2 = 20;
-n1 = "1" + 5;
-console.log(n1 + n2);
---
+++ JS4T/operadores.js
@@ -0,0 +1,38 @@
+//Os operadores vão nos permitir fazer operações matemáticas de comparação e lógicas
+
+//Aritiméticos
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+
+//Comparação
+//  console.log(2 == 2);
+//  console.log(2 == 3);
+//  console.log(2 != 1);
+//console.log(2 == "2"); //retorna true porque os valores são iguais independente do tipo 
+
+ //Igual estrito
+//  console.log(2 === "2");
+ 
+ //Não igual estrito
+//  console.log(2 !== "2");
+//  console.log("2" !== "2");
+
+ //Maior que (>)
+//  console.log(2 > 3);
+//  console.log(2 > 1);
+
+//Menor que (<)
+// console.log(2 < 3);
+// console.log(2 < 1);
+
+ //Maior igual que (>=)
+//  console.log(2 >= 3);
+//  console.log(2 >= 2);
+//  console.log(2 >= 1);
+
+ //Menor igual que (<=)
+// console.log(2 <= 3);
+// console.log(2 <= 2);
+// console.log(2 <= 1);
