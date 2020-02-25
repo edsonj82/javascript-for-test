094e5b842b6275327cc2506c955ea2305b608a44
Edson <edsonj82@outlook.com>
2020-02-24 22:41:39 -0300

    Funções

---
+++ JS4T/arrays.js
@@ -0,0 +1,29 @@
+
+//var arangers = new Array();
+var avangers = ['homem de ferro', 'capitão america', 'homem aranha'];
+console.log(avangers);
+
+avangers.push('huck');
+console.log(avangers);
+
+avangers.push('capitão cueca');
+console.log(avangers);
+
+// avangers.pop()//pop remove o ultimo registro do meu array
+// console.log(avangers);
+
+// avangers.shift()//shift remove o meu primeiro registro do meu array
+// console.log(avangers);
+
+var indice = avangers.indexOf('capitão cueca');
+avangers.splice(indice)
+console.log(avangers)
+
+avangers.push('Viuva Negra');
+avangers.push('Gavião Arqueiro');
+console.log(avangers)
+
+var newAvangers = ['homem aranha3', 'Capitã marvel', 'miss marvel'];
+
+var result = avangers.concat(newAvangers);
+console.log(result);
--- JS4T/main.js
+++ JS4T/main.js
@@ -1,32 +1,8 @@
-var welcome = ".::Hello Javascript - Automação Full Stack from QA Ninja::."
-document.getElementById("welcome").append(welcome);
-console.log(welcome);
+function welcome(params) {
+    var welcome = ".::Hello Javascript - Automação Full Stack from QA Ninja::."
+    document.getElementById("welcome").append(welcome);
+    console.log(welcome);
+}
 
-//var arangers = new Array();
-var avangers = ['homem de ferro', 'capitão america', 'homem aranha'];
-console.log(avangers);
+welcome();
 
-avangers.push('huck');
-console.log(avangers);
-
-avangers.push('capitão cueca');
-console.log(avangers);
-
-// avangers.pop()//pop remove o ultimo registro do meu array
-// console.log(avangers);
-
-// avangers.shift()//shift remove o meu primeiro registro do meu array
-// console.log(avangers);
-
-var indice = avangers.indexOf('capitão cueca');
-avangers.splice(indice)
-console.log(avangers)
-
-avangers.push('Viuva Negra');
-avangers.push('Gavião Arqueiro');
-console.log(avangers)
-
-var newAvangers = ['homem aranha3', 'Capitã marvel', 'miss marvel'];
-
-var result = avangers.concat(newAvangers);
-console.log(result);
