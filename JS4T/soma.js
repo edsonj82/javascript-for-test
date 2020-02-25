45f8a6203f032535d9e6910c649d9e1435fa117a
Edson <edsonj82@outlook.com>
2020-02-25 00:08:45 -0300

    Document Object Model

---
+++ JS4T/soma.js
@@ -0,0 +1,11 @@
+//DOM - Document Object Model
+function somarValores() {
+    var n1 = document.getElementById('number1').value;
+    var n2 = document.getElementById('number2').value;
+
+    var result = parseInt(n1) + parseInt(n2);
+
+    var divResultado = document.getElementById('resultado');
+    divResultado.append("O resultado da soma é " + result);
+    console.log(result);
+}
